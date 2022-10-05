9952b2536a7ee367c2bcc6e12f2b887a35bdccbe
Jeremiah <jbcabral24@gmail.com>
2022-10-05 11:28:55 +0800

    first commit

---
+++ Timelog.Agent2/ViewModels/Log.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timelog.Agent.ViewModels
+{
+    class Log
+    {
+        public string EnrolleeId { get; set; }
+        public string DeviceId { get; set; }
+        public DateTimeOffset TimeLog { get; set; }
+    }
+}
